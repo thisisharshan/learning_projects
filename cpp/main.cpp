1c59ab6e8c52169a1779b8153e62747747facef1
friction-breaker <harshanprabhu01@gmail.com>
2026-01-04 20:35:49 +0530

    updates

---
+++ cpp/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int a,b,s;
+    char z,x,c,v;
+
+    cout << "TUI calculator" << "\n";
+    cout << endl;
+
+    cout << "Enter: "; cin >> a;
+    cout << "Enter: "; cin >> b;
+
+    cout << "Add, Subtract, multiply, Divide, Modulus" << "\n";
+    cout << "choose z,x,c,v,b" << "\n";
+    cout << endl;
+
+    
+}
--- python/ternary-expression.py
+++ python/ternary-expression.py
@@ -1,9 +1,13 @@
+#Syntax - X if condition else Y
+
 #example 1 - ticket checker
 
-ticket = [100,120,171]
-chk = int(input("What is your ticket number?\t"))
-access = "Allowed\n" if chk == ticket else "Not allowed"
-print (access)
+# ticket = 100
+# chk = int(input("What is your ticket number?\t"))
+# access = "Allowed\n" if chk == ticket else "Not allowed"
+# print (access)
+
+#example 2 - Temperature checker
 
 # temp = int(input("What is the temperature currently?\t"))
 # print("The temperature is hot") if temp > 30 else print("The temperature is cool")
